19921d0c222963ff8ed999f6fc549f3db7a4c7b2
diem.nguyen <diem.nguyen@team.enouvo.com>
2023-03-02 14:54:07 +0700

    add logout

--- login.ts
+++ login.ts
@@ -2,6 +2,9 @@ class LoginPage {
   get loginBtn(): string {
     return "//span[text()='Log In']//parent::button";
   }
+  get logout(): string {
+    return "//span[text()='Log In']//parent::button";
+  }
   get loginAsAParent(): string {
     return "//span[text()='As a Parent']";
   }
